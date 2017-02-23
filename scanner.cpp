e69a0926af67eb335e9b2a68923afadd756ba21f
Harsh Shah <harsh.shah.jayesh@gmail.com>
2017-02-23 17:48:51 +0530

    Small scanner

---
+++ scanner.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Token {
+    public:
+        string type;
+        string value;
+};
+
+Token scanner();
+Token scan_digits();
+bool is_identifier(char ch);
+
+string s;
+int i = 0;
+
+int main()
+{
+    getline(cin, s);
+    Token ans = scanner();
+    while (ans.type != "$") {
+        ans = scanner();
+    }
+    return 0;
+}
+
+Token scanner()
+{
+    Token ans;
+    if (s[i] == ' ') {
+        i++;
+    }
+    if (i == s.length()) {
+        ans.type = "$";
+    } else {
+        if (s[i] >= '0' && s[i] <= '9') {
+            ans = scan_digits();
+        } else {
+            char ch = s[i];
+            i++;
+            if (is_identifier(ch)) {
+                ans.type = "id";
+                ans.value = string (1, ch);
+            } else if (ch == 'f') {
+                ans.type = "floatdcl";
+            } else if (ch == 'i') {
+                ans.type = "intdcl";
+            } else if (ch == 'p') {
+                ans.type = "print";
+            } else if (ch == '=') {
+                ans.type = "assign";
+            } else if (ch == '+') {
+                ans.type = "plus";
+            } else if (ch == '-') {
+                ans.type = "minus";
+            } else {
+                cout << "LEXICAL ERROR" << endl;
+            }
+        }
+    }
+    cout << ans.type << " " << ans.value << endl;
+    return ans;
+}
+
+Token scan_digits(void)
+{
+    Token ans;
+    ans.value = "";
+
+    while (s[i] >= '0' && s[i] <= '9') {
+        ans.value = ans.value + string(1, s[i]);
+        i++;
+    }
+    if (s[i] != '.') {
+        ans.type = "inum";
+    } else {
+        ans.type = "fnum";
+        ans.value += ".";
+        i++;
+        while (s[i] >= '0' && s[i] <= '9') {
+            ans.value = ans.value + string(1, s[i]);
+            i++;
+        }
+    }
+    return ans;
+}
+
+bool is_identifier(char ch) 
+{
+    if (ch >= 'a' && ch <= 'z' && ch != 'i' && ch != 'f' && ch != 'p') {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+
