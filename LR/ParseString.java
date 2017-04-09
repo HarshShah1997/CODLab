5caf6bc5f5fb167c110f79b1899f9a8733ddd7b1
Harsh Shah <harsh.shah.jayesh@gmail.com>
2017-04-09 17:02:07 +0530

    Completed Parsing String

--- LR/ParseString.java
+++ LR/ParseString.java
@@ -33,6 +33,58 @@ class ParseString {
     void run() {
         checkInput();
         table = (new LR0Table(grammer)).buildTable();
+
+        boolean result = parse();
+        System.out.println(result);
+    }
+
+    boolean parse() {
+        Stack<String> stack = new Stack<String>();
+        stack.push("0");
+
+        int pointer = 0;
+        boolean accept = false;
+        while (!stack.empty()) {
+            int top = Integer.parseInt(stack.peek());
+            boolean last = false;
+            if (pointer == inputString.length()) {
+                pointer--;
+                last = true;
+            }
+            Pair<Integer,String> key = new Pair<Integer,String>
+                (top, ""+inputString.charAt(pointer));
+            if (table.get(key) == null) {
+                break;
+            }
+            String value = table.get(key);
+            System.out.println(stack);
+            if (last) {
+                if (value.equals("accept")) {
+                    accept = true;
+                }
+                break;
+            }
+            if (value.charAt(0) == 'S') {
+                stack.push(""+inputString.charAt(pointer));
+                pointer++;
+                stack.push(""+value.charAt(1));
+            } else if (value.charAt(0) == 'R') {
+                int index = value.charAt(1) - '0';
+                Production reduce = allProductions.get(index);
+                popFromStack(stack, reduce.body.length() * 2);
+
+                int prev = Integer.parseInt(stack.peek());
+                stack.push(reduce.head.name);
+
+                Pair<Integer,String> newkey = new Pair<Integer,String>
+                    (prev, reduce.head.name);
+                if (table.get(newkey) == null) {
+                    break;
+                }
+                stack.push(table.get(newkey));
+            }
+        }
+        return accept;
     }
 
     void checkInput() {
@@ -49,6 +101,12 @@ class ParseString {
         } catch (Exception ex) {
             ex.printStackTrace();
             System.exit(1);
+        }
+    }
+
+    void popFromStack(Stack<String> stack, int times) {
+        for (int i = 0; i < times; i++) {
+            stack.pop();
         }
     }
 
